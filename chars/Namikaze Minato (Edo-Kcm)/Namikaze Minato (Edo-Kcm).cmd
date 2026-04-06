3304c84ca561675ad467c0d40351ab16938b2b79
Renzo Sparta <exequiel0210@gmail.com>
2026-04-06 02:10:27 +0000

    Sync with windows chars, screenpack and stages

---
+++ chars/Namikaze Minato (Edo-Kcm)/Namikaze Minato (Edo-Kcm).cmd
@@ -0,0 +1,643 @@
+
+
+
+[Remap]
+x = x
+y = y
+z = z
+a = a
+b = b
+c = c
+s = s
+
+[command]
+name = "SPECIAL 1"
+command = ~D,DF,F,a
+time = 15
+
+[command]
+name = "SPECIAL 2"
+command = ~D,DB,B,a
+time = 15
+
+[command]
+name = "SPECIAL 3"
+command = ~D,DF,F,b
+time = 15
+
+[command]
+name = "SPECIAL 4"
+command = ~D,DB,B,b
+time = 15
+
+[command]
+name = "SPECIAL 5"
+command = ~D,DF,F,c
+time = 15
+
+[command]
+name = "SPECIAL 6"
+command = ~D,DB,B,c
+time = 15
+
+[command]
+name = "SPECIAL 7"
+command = ~D,DF,F,s
+time = 15
+
+[command]
+name = "SPECIAL 8"
+command = ~D,DB,B,s
+time = 15
+
+[Defaults]
+
+
+[Command]
+name = "FF"
+command = F, F
+time = 10
+
+[Command]
+name = "BB"
+command = B, B
+time = 10
+
+[Command]
+name = "recovery"
+command = x+y
+time = 1
+
+[Command]
+name = "recovery"
+command = y+z
+time = 1
+
+[Command]
+name = "recovery"
+command = x+z
+time = 1
+
+[Command]
+name = "recovery"
+command = a+b
+time = 1
+
+[Command]
+name = "recovery"
+command = b+c
+time = 1
+
+[Command]
+name = "recovery"
+command = a+c
+time = 1
+
+[Command]
+name = "back_x"
+command = /$B,x
+time = 1
+
+[Command]
+name = "back_y"
+command = /$B,y
+time = 1
+
+[Command]
+name = "back_z"
+command = /$B,z
+time = 1
+
+[Command]
+name = "down_x"
+command = /$D,x
+time = 1
+
+[Command]
+name = "down_y"
+command = /$D,y
+time = 1
+
+[Command]
+name = "down_z"
+command = /$D,z
+time = 1
+
+[Command]
+name = "fwd_x"
+command = /$F,x
+time = 1
+
+[Command]
+name = "fwd_y"
+command = /$F,y
+time = 1
+
+[Command]
+name = "fwd_z"
+command = /$F,z
+time = 1
+
+[Command]
+name = "up_x"
+command = /$U,x
+time = 1
+
+[Command]
+name = "up_y"
+command = /$U,y
+time = 1
+
+[Command]
+name = "up_z"
+command = /$U,z
+time = 1
+
+[Command]
+name = "back_a"
+command = /$B,a
+time = 1
+
+[Command]
+name = "back_b"
+command = /$B,b
+time = 1
+
+[Command]
+name = "back_c"
+command = /$B,c
+time = 1
+
+[Command]
+name = "down_a"
+command = /$D,a
+time = 1
+
+[Command]
+name = "down_b"
+command = /$D,b
+time = 1
+
+[Command]
+name = "down_c"
+command = /$D,c
+time = 1
+
+[Command]
+name = "fwd_a"
+command = /$F,a
+time = 1
+
+[Command]
+name = "fwd_b"
+command = /$F,b
+time = 1
+
+[Command]
+name = "fwd_c"
+command = /$F,c
+time = 1
+
+[Command]
+name = "up_a"
+command = /$U,a
+time = 1
+
+[Command]
+name = "up_b"
+command = /$U,b
+time = 1
+
+[Command]
+name = "up_c"
+command = /$U,c
+time = 1
+
+[Command]
+name = "a"
+command = a
+time = 1
+
+[Command]
+name = "b"
+command = b
+time = 1
+
+[Command]
+name = "c"
+command = c
+time = 1
+
+[Command]
+name = "x"
+command = x
+time = 1
+
+[Command]
+name = "y"
+command = y
+time = 1
+
+[Command]
+name = "z"
+command = z
+time = 1
+
+[Command]
+name = "s"
+command = s
+time = 1
+
+[Command]
+name = "fwd"
+command = $F
+time = 1
+
+[Command]
+name = "downfwd"
+command = $DF
+time = 1
+
+[Command]
+name = "down"
+command = $D
+time = 1
+
+[Command]
+name = "downback"
+command = $DB
+time = 1
+
+[Command]
+name = "back"
+command = $B
+time = 1
+
+[Command]
+name = "upback"
+command = $UB
+time = 1
+
+[Command]
+name = "up"
+command = $U
+time = 1
+
+[Command]
+name = "upfwd"
+command = $UF
+time = 1
+
+[Command]
+name = "hold_x"
+command = /x
+time = 1
+
+[Command]
+name = "hold_y"
+command = /y
+time = 1
+
+[Command]
+name = "hold_z"
+command = /z
+time = 1
+
+[Command]
+name = "hold_a"
+command = /a
+time = 1
+
+[Command]
+name = "hold_b"
+command = /b
+time = 1
+
+[Command]
+name = "hold_c"
+command = /c
+time = 1
+
+[Command]
+name = "hold_s"
+command = /s
+time = 1
+
+[Command]
+name = "holdfwd"
+command = /$F
+time = 1
+
+[Command]
+name = "holddownfwd"
+command = /$DF
+time = 1
+
+[Command]
+name = "holddown"
+command = /$D
+time = 1
+
+[Command]
+name = "holddownback"
+command = /$DB
+time = 1
+
+[Command]
+name = "holdback"
+command = /$B
+time = 1
+
+[Command]
+name = "holdupback"
+command = /$UB
+time = 1
+
+[Command]
+name = "holdup"
+command = /$U
+time = 1
+
+[Command]
+name = "holdupfwd"
+command = /$UF
+time = 1
+
+
+[Command]
+name = "AI Prueba"
+command = y+z
+time = 1
+
+[Command]
+name = "AI desactivado"
+command = b+c
+time = 1
+
+
+[command]
+name = "AI1"
+command = b,b,b,b,b,b,b,b,b,b,b
+time = 1
+
+[command]
+name = "AI2"
+command = b,c,b,c,b,c,b,c,b,c,b,c
+time = 1
+
+[command]
+name = "AI3"
+command = b,c,b,c,b,c,b,c,b,c,b
+time = 1
+
+[command]
+name = "AI4"
+command = a,b,a,b,a,b,a,b,a,b,a
+time = 1
+
+[command]
+name = "AI5"
+command = b,a,b,a,b,a,b,a,b,a,b
+time = 1
+
+[command]
+name = "AI6"
+command = b,b,b,b,z,b,z,b,z,z,z
+time = 1
+
+[command]
+name = "AI7"
+command = b,x,b,b,x,b,b,b,b,b,x
+time = 1
+
+[command]
+name = "AI8"
+command = b,y,b,b,b,y,b,b,b,b,y
+time = 1
+
+[command]
+name = "AI9"
+command = b,b,b,b,s,z,y,x,c,b,a
+time = 1
+
+[command]
+name = "AI10"
+command = z,b,x,y,b,b,c,b,b,b,a
+time = 1
+
+[command]
+name = "AI11"
+command = b,x,b,b,x,b,b,b,b,b,z
+time = 1
+
+[command]
+name = "AI12"
+command = b,y,b,s,b,y,b,b,b,b,y
+time = 1
+
+[command]
+name = "AI13"
+command = b,b,b,z,s,z,y,x,c,b,a
+time = 1
+
+[command]
+name = "AI14"
+command = z,b,x,y,b,b,c,b,b,c,a
+time = 1
+;---------------------------------------------------------------------------
+[Statedef -1]
+;===========================================================================
+;---------------------------Basicos-----------------------------------------
+;---------------------------------------------------------------------------
+; FDash
+[State -1, FDash]
+type = ChangeState
+triggerall = stateno != 60
+triggerall = stateno != 70
+value = 60
+trigger1 = command = "FF"
+trigger1 = statetype != A
+trigger1 = ctrl
+
+; Bdash
+[State -1, BDash]
+type = ChangeState
+triggerall = stateno != 60
+triggerall = stateno != 70
+value = 70
+trigger1 = command = "BB"
+trigger1 = statetype != A
+trigger1 = ctrl
+
+[State -1, Correr Adelante]
+type = ChangeState
+triggerall = stateno != 65
+triggerall = stateno != 75
+value = 65
+trigger1 = command = "FF"
+trigger1 = statetype = A
+trigger1 = ctrl
+
+[State -1, Correr Atras]
+type = ChangeState
+triggerall = stateno != 65
+triggerall = stateno != 75
+value = 75
+trigger1 = command = "BB"
+trigger1 = statetype = A
+trigger1 = ctrl
+
+;---------------------------------------------------------------------------
+
+[State -1, Rasenga]
+type = ChangeState
+Triggerall = power >= 1000
+Triggerall = command = "SPECIAL 1"
+Triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 1000
+
+[State -1, Shuriken Kage Bushin no Jutsu]
+type = ChangeState
+Triggerall = numhelper(1110) = 0
+Triggerall = numhelper(1149) = 0
+Triggerall = numhelper(1150) = 0
+Triggerall = power >= 500
+triggerall = command = "SPECIAL 2"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 1100
+
+[State -1, Hiraishin no Jutsu - Seal Marking]
+type = ChangeState
+Triggerall = numhelper(1299) = 0
+Triggerall = power >= 250
+triggerall = command = "SPECIAL 3"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 1200
+
+[State -1, Hiraishin no Jutsu : Chase]
+type = ChangeState
+Triggerall = numhelper(1299) >= 1
+Triggerall = power >= 500
+triggerall = command = "SPECIAL 3"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 1250
+
+[State -1, Hisho Gekkorin Rasen Shippu Zeroshiki]
+type = ChangeState
+Triggerall = power >= 500
+triggerall = command = "SPECIAL 4"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 1300
+
+[State -1, Kurama Punch]
+type = ChangeState
+Triggerall = power >= 500
+triggerall = command = "SPECIAL 5"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 1400
+
+[State -1, Celestail Air Slice Type-Zero]
+type = ChangeState
+Triggerall = power >= 1500
+triggerall = command = "holddown"
+triggerall = command = "a"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 1500
+
+[State -1, Kurama's triple tail attack]
+type = ChangeState
+Triggerall = power >= 1000
+triggerall = command = "SPECIAL 6"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 3000
+
+[State -1, Rasen senko cho ribuko sanshiki]
+type = ChangeState
+Triggerall = power >= 2000
+triggerall = command = "holddown"
+triggerall = command = "c"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 3100
+
+[State -1, Bijuu Dama]
+type = ChangeState
+Triggerall = power >= 2000
+triggerall = command = "holddown"
+triggerall = command = "b"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = numhelper(700) > 0
+value = 3200
+
+[State -1, Combo 1]
+type = ChangeState
+triggerall = command = "a"
+trigger1 = statetype != A
+trigger1 = ctrl
+value = 200
+
+[State -1, Combo 2]
+type = ChangeState
+triggerall = command = "b"
+trigger1 = statetype != A
+trigger1 = ctrl
+value = 300
+
+[State -1, Combo 3]
+type = ChangeState
+triggerall = command = "c"
+trigger1 = statetype != A
+trigger1 = ctrl
+value = 400
+
+[State -1, Power Charge]
+type = ChangeState
+Triggerall = power < 3000
+triggerall = command = "s"
+trigger1 = statetype != A
+trigger1 = ctrl
+value = 500
+
+[State -1, Awakening]
+type = ChangeState
+Triggerall = life <= lifemax/1
+Triggerall = var(2) = 0
+Triggerall = power = 3000
+triggerall = command = "s"
+trigger1 = statetype != A
+trigger1 = ctrl
+value = 550
+
+[State -1, Combo Aire]
+type = ChangeState
+triggerall = command = "a"
+trigger1 = statetype = A
+trigger1 = ctrl
+value = 600
+
+; Combo Aire2
+[State -1, Combo Aire]
+type = ChangeState
+triggerall = pos y < -60
+triggerall = command = "b"||command = "c"
+trigger1 = statetype = A
+trigger1 = ctrl
+value = 650
---
+++ data/remix/README.txt
@@ -0,0 +1,82 @@
+﻿                         __________________________________________________
+========================| MUGEN1 Screenpack Remix by Phantom.of.the.Server |========================
+                         ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯       [24.07.2024]
+
+ - Contact: potsmugen@gmail.com
+ - Website: https://network.mugenguild.com/pots/
+ - If you downloaded this file from anywhere else, it's probably outdated
+ 
+ - Custom screenpack for Ikemen GO 0.99 and above
+ - For older versions for older engines, try the Mediafire folder
+
+
+
+=========================================< INSTALLATION >==========================================
+
+ 1 - Open your Ikemen GO's "data" folder
+
+ 2 - Create a new subfolder called "mugen1_screenpack_remix"
+
+ 3 - Move the contents of the archive into the new folder
+
+ 4 - Inside Ikemen's "save" folder, open the "config.json" file with a text editor
+
+ 5 - Look for this line:
+
+  "Motif": "data/system.def",
+
+ 6A - For 16:9 screen aspect ratio, replace the line with this:
+
+  "Motif": "data/mugen1_screenpack_remix/system_169.def",
+
+ 6B - For 4:3 screen aspect ratio, replace the line with this:
+
+  "Motif": "data/mugen1_screenpack_remix/system_43.def",
+
+
+
+=========================================< INTRODUCTION >==========================================
+
+This is an enhanced edit of Ikemen GO's default high resolution screenpack.
+
+It features a new color scheme, new layouts, menu music and some Ikemen GO features not actually found in the original.
+
+
+
+============================================< F.A.Q. >=============================================
+
+Q: Can I use this screenpack with Orange-Dolphin's palette select branch?
+|
+A: Yes, you can. It is already configured for that, so all you need to do is install the screenpack.
+
+
+
+=========================================< KNOWN ISSUES >==========================================
+
+
+
+=======================================< VERSION HISTORY >=========================================
+
+<24.07.2024>
+ - Adjusted main menu layout in 4:3 mode
+ - Added support for Orange-Dolphin's palette select branch
+ - Added a scrolling background behind the Options menu title, like in other screens
+
+<19.07.2024>
+ - First release
+
+
+
+========================================< WHAT'S MISSING >=========================================
+
+ - Some polish
+
+
+
+========================================< SPECIAL THANKS >=========================================
+
+ - Elecbyte, for the original MUGEN1 screenpack
+ - Ikemen GO team, for several additions to the MUGEN1 screenpack
+ - fantasma, for the "Kung Fu Gopher" image
+ - Super, for making the Third Heights stage used in one of the backgrounds
+ - Soundraw, for their music generation website
